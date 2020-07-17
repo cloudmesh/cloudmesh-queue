e7bfd3c63ad285751d19634dfa9369fbe74ec53c
Gregor von Laszewski <laszewski@gmail.com>
2020-07-17 19:48:41 -0400

    add the manual page

--- cloudmesh/job/command/job.py
+++ cloudmesh/job/command/job.py
@@ -15,8 +15,15 @@ class JobCommand(PluginCommand):
         ::
 
           Usage:
-                job --file=FILE
-                job list
+            job set FILE
+            job add FILE
+            job status
+            job list --status=STATUS
+            job list --name=NAME
+            job list
+            job kill --name=NAME
+            job reset --name=NAME
+            job delete --name=NAME
 
           This command does some useful things.
 
@@ -25,17 +32,42 @@ class JobCommand(PluginCommand):
 
           Options:
               -f      specify the file
+              --status=STATUS  the status [default: None]
 
           Description:
+
+              job set FILE
+                sets the jobset to the file name. All other commands will be
+                applied to a jobset
+
+              job add FILE
+                adds the jobs in the file to the jobset
 
               job list
                 lists all jobs
 
               job list --status=open
-                lists alsl jobs with a specific status
+                lists all jobs with a specific status
+
+              job list --name=NAME
+                lists teh job with the given name pattern
 
               job status
                 shows the status of all jobs
+
+              job kill --name=NAME
+                kills the given jobs base on a name pattern such as
+                name[01-04] which would kill all jobs with the given names
+
+              job status --name=NAME --status=STATUS
+                sets the status of all jobs to the status
+
+              job reset --name=NAME
+                resets the job to be rerun
+
+              job delete --name=NAME
+                deletes the given jobs base on a name pattern such as
+                name[01-04] which would kill all jobs with the given names
 
           Job States:
 
@@ -43,22 +75,63 @@ class JobCommand(PluginCommand):
              ready  - ready for scheduling
              failed - job failed
 
-          more to come
-          
+          Job specification:
+
+             The jobs are specified in aa yaml file
+
+             name:
+               ip: ip of the host
+               input: where the input comes form (locally to ip)
+               output: where to store the outout (locally to ip)
+               status: the status
+               gpu: the GPU specification # string such as "0,2" as defined by the
+                    GPU framework e.g. NVIDIA
+               user: the userneme on ip to execute the job
+               directory: the working directory
+               arguments: the arguments passed along # lis of key values
+               executable: the executable
+               shell: bash # executes the job in the provided shell
+                      $(SHELL) will use the default user shell
         """
 
-        #arguments.FILE = arguments['--file'] or None
+        if arguments.set:
+            # job set FILE
+            file = arguments["FILE"]
+            Console.error("Not yet implemented")
 
-        #VERBOSE(arguments)
+        elif arguments.add:
+            # job add FILE
+            Console.error("Not yet implemented")
 
+        elif arguments.status:
+            #job status
+            Console.error("Not yet implemented")
 
-        #if arguments.FILE:
-        #    print("option a")
-        #    m.list(path_expand(arguments.FILE))
-        #
-        #elif arguments.list:
-        #    print("option b")
-        #    m.list("just calling list without parameter")
+        elif arguments.list and arguments["--status"]:
+            #job list --status=STATUS
+            Console.error("Not yet implemented")
 
-        Console.error("Not yet implemented")
+        elif arguments.list and arguments["--name"]:
+            ##job list --name=NAME
+            Console.error("Not yet implemented")
+
+        elif arguments.list:
+            #job list
+            Console.error("Not yet implemented")
+
+        elif arguments.kill:
+            #job kill --name=NAME
+            name = arguments["--name"]
+            Console.error("Not yet implemented")
+
+        elif arguments.reset:
+            #job reset --name=NAME
+            name = arguments["--name"]
+            Console.error("Not yet implemented")
+
+        elif arguments.delete:
+            #job delete --name=NAME
+            name = arguments["--name"]
+            Console.error("Not yet implemented")
+            
         return ""
