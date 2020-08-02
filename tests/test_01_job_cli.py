6268e6663a4c3f31060a3961802fd57109716d58
Gregor von Laszewski <laszewski@gmail.com>
2020-08-02 15:36:43 -0400

    merge

--- tests/test_01_job_cli.py
+++ tests/test_01_job_cli.py
@@ -7,7 +7,7 @@ import pytest
 from cloudmesh.common.Shell import Shell
 from cloudmesh.common.debug import VERBOSE
 from cloudmesh.common.util import HEADING
-from cloudmesh.common3.Benchmark import Benchmark
+from cloudmesh.common.Benchmark import Benchmark
 from cloudmesh.common.variables import Variables
 from cloudmesh.configuration.Config import Config
 from textwrap import dedent
